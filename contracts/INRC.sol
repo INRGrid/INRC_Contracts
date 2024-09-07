b04f907060b4b954b0ba5561499a429e33704869
Aman Yadav <goku.amyadav@gmail.com>
2024-09-07 16:27:10 +0530

    Contracts Updates

--- contracts/INRC.sol
+++ contracts/INRC.sol
@@ -5,7 +5,9 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
+import "./INRC_Token.sol";
+import "./INRC_Permit.sol";
+import "./INRC_Burnable.sol";
 
 import "hardhat/console.sol";
 
